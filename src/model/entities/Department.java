2b69640b6adcdf80f877adfee40f9cd6d34afcfa
Danrleydev <danrleysouza53@gmail.com>
2022-08-29 11:29:17 -0300

    finalizando projeto

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -1,21 +1,20 @@
 package model.entities;
 
 import java.io.Serializable;
-import java.util.Objects;
 
 public class Department implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
 	private Integer id;
-	private String nome;
-
+	private String name;
+	
 	public Department() {
 	}
 
-	public Department(Integer id, String nome) {
+	public Department(Integer id, String name) {
 		this.id = id;
-		this.nome = nome;
+		this.name = name;
 	}
 
 	public Integer getId() {
@@ -26,17 +25,20 @@ public class Department implements Serializable {
 		this.id = id;
 	}
 
-	public String getNome() {
-		return nome;
+	public String getName() {
+		return name;
 	}
 
-	public void setNome(String nome) {
-		this.nome = nome;
+	public void setName(String name) {
+		this.name = name;
 	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -48,12 +50,16 @@ public class Department implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Department other = (Department) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 
 	@Override
 	public String toString() {
-		return "Department [id=" + id + ", nome=" + nome + "]";
+		return "Department [id=" + id + ", name=" + name + "]";
 	}
-
 }
